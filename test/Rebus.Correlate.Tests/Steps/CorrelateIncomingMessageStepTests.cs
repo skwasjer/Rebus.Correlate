ff9b82e9378f156f7239ca934e4b8476548bd51b
skwas <skwas@skwas.net>
2019-07-28 08:57:33 +0200

    Cover steps with unit tests (currently only covered by integration tests)

---
+++ test/Rebus.Correlate.Tests/Steps/CorrelateIncomingMessageStepTests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Correlate;
+using Moq;
+using Rebus.Logging;
+using Rebus.Messages;
+using Rebus.Pipeline;
+using Rebus.Transport;
+using Xunit;
+
+namespace Rebus.Correlate.Steps
+{
+	public class CorrelateIncomingMessageStepTests
+	{
+		private readonly Mock<IAsyncCorrelationManager> _asyncCorrelationManagerMock;
+		private readonly Dictionary<string, string> _messageHeaders;
+		private readonly IncomingStepContext _stepContext;
+		private readonly Func<Task> _next;
+		private readonly CorrelateIncomingMessageStep _sut;
+
+		public CorrelateIncomingMessageStepTests()
+		{
+			_asyncCorrelationManagerMock = new Mock<IAsyncCorrelationManager>();
+
+			var txItems = new ConcurrentDictionary<string, object>();
+			var transactionContextMock = new Mock<ITransactionContext>();
+			transactionContextMock
+				.Setup(m => m.Items)
+				.Returns(txItems);
+
+			_messageHeaders = new Dictionary<string, string>();
+			var transportMessage = new TransportMessage(_messageHeaders, new byte[0]);
+			_stepContext = new IncomingStepContext(transportMessage, transactionContextMock.Object);
+			_stepContext.Save(new Message(_messageHeaders, new { }));
+
+			_next = () => Task.CompletedTask;
+
+			_sut = new CorrelateIncomingMessageStep(_asyncCorrelationManagerMock.Object, new NullLoggerFactory());
+		}
+
+		[Fact]
+		public async Task Given_no_correlation_id_is_stored_with_message_it_should_use_message_id()
+		{
+			const string expectedCorrelationId = null;
+			_messageHeaders.Clear();
+
+			// Act
+			await _sut.Process(_stepContext, _next);
+
+			// Assert
+			_asyncCorrelationManagerMock.Verify(m => m.CorrelateAsync(expectedCorrelationId, _next, It.IsAny<OnException>()), Times.Once);
+		}
+
+		[Fact]
+		public async Task Given_correlation_id_is_stored_with_message_it_should_use_header_value()
+		{
+			const string correlationId = nameof(correlationId);
+			const string expectedCorrelationId = correlationId;
+			_messageHeaders.Add(Headers.CorrelationId, correlationId);
+
+			// Act
+			await _sut.Process(_stepContext, _next);
+
+			// Assert
+			_asyncCorrelationManagerMock.Verify(m => m.CorrelateAsync(expectedCorrelationId, _next, It.IsAny<OnException>()), Times.Once);
+		}
+	}
+}
---
+++ test/Rebus.Correlate.Tests/Steps/CorrelateOutgoingMessageStepTests.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Correlate;
+using FluentAssertions;
+using Moq;
+using Rebus.Logging;
+using Rebus.Messages;
+using Rebus.Pipeline;
+using Rebus.Pipeline.Send;
+using Rebus.Transport;
+using Xunit;
+
+namespace Rebus.Correlate.Steps
+{
+	public class CorrelateOutgoingMessageStepTests
+	{
+		private readonly CorrelationContextAccessor _correlationContextAccessor;
+		private readonly Mock<ICorrelationIdFactory> _correlationIdFactoryMock;
+		private readonly Mock<ITransactionContext> _transactionContextMock;
+		private readonly Dictionary<string, string> _messageHeaders;
+		private readonly OutgoingStepContext _stepContext;
+		private readonly Func<Task> _next;
+		private readonly CorrelateOutgoingMessageStep _sut;
+
+		public CorrelateOutgoingMessageStepTests()
+		{
+			_correlationContextAccessor = new CorrelationContextAccessor();
+			_correlationIdFactoryMock = new Mock<ICorrelationIdFactory>();
+
+			var txItems = new ConcurrentDictionary<string, object>();
+			_transactionContextMock = new Mock<ITransactionContext>();
+			_transactionContextMock
+				.Setup(m => m.Items)
+				.Returns(txItems);
+
+			_messageHeaders = new Dictionary<string, string>();
+			_stepContext = new OutgoingStepContext(new Message(_messageHeaders, new { }), _transactionContextMock.Object, new DestinationAddresses(Enumerable.Empty<string>()));
+
+			_next = () => Task.CompletedTask;
+
+			_sut = new CorrelateOutgoingMessageStep(_correlationContextAccessor, _correlationIdFactoryMock.Object, new NullLoggerFactory());
+		}
+
+		[Fact]
+		public async Task Given_message_already_has_a_correlation_id_stored_it_should_ignore_setting_correlation_id()
+		{
+			const string correlationId = nameof(correlationId);
+			const string expectedCorrelationId = correlationId;
+			_messageHeaders.Add(Headers.CorrelationId, correlationId);
+			_correlationContextAccessor.CorrelationContext = null;
+
+			bool isNextCalled = false;
+
+			Task Next()
+			{
+				isNextCalled = true;
+				return Task.CompletedTask;
+			}
+
+			// Act
+			await _sut.Process(_stepContext, Next);
+
+			// Assert
+			_messageHeaders
+				.Should().ContainKey(Headers.CorrelationId)
+				.WhichValue
+				.Should().Be(expectedCorrelationId);
+			_correlationIdFactoryMock.Verify(m => m.Create(), Times.Never);
+			isNextCalled.Should().BeTrue();
+		}
+
+		[Fact]
+		public async Task Given_message_has_no_correlation_id_and_correlation_context_is_available_it_should_use_correlation_id_from_context()
+		{
+			const string correlationId = nameof(correlationId);
+			const string expectedCorrelationId = correlationId;
+			_messageHeaders.Clear();
+			_correlationContextAccessor.CorrelationContext = new CorrelationContext
+			{
+				CorrelationId = correlationId
+			};
+
+			bool isNextCalled = false;
+
+			Task Next()
+			{
+				isNextCalled = true;
+				return Task.CompletedTask;
+			}
+
+			// Act
+			await _sut.Process(_stepContext, Next);
+
+			// Assert
+			_messageHeaders
+				.Should().ContainKey(Headers.CorrelationId)
+				.WhichValue
+				.Should().Be(expectedCorrelationId);
+			_correlationIdFactoryMock.Verify(m => m.Create(), Times.Never);
+			isNextCalled.Should().BeTrue();
+		}
+
+		[Fact]
+		public async Task Given_message_has_no_correlation_id_and_correlation_context_is_not_available_it_should_use_correlation_id_from_factory()
+		{
+			const string correlationId = nameof(correlationId);
+			const string expectedCorrelationId = correlationId;
+			_messageHeaders.Clear();
+			_correlationContextAccessor.CorrelationContext = null;
+			_correlationIdFactoryMock
+				.Setup(m => m.Create())
+				.Returns(correlationId)
+				.Verifiable();
+
+			bool isNextCalled = false;
+
+			Task Next()
+			{
+				isNextCalled = true;
+				return Task.CompletedTask;
+			}
+
+			// Act
+			await _sut.Process(_stepContext, Next);
+
+			// Assert
+			_messageHeaders
+				.Should().ContainKey(Headers.CorrelationId)
+				.WhichValue
+				.Should().Be(expectedCorrelationId);
+			_correlationIdFactoryMock.Verify();
+			isNextCalled.Should().BeTrue();
+		}
+
+		[Fact]
+		public async Task Given_no_incoming_step_context_it_should_start_new_sequence()
+		{
+			// Act
+			await _sut.Process(_stepContext, _next);
+
+			// Assert
+			_messageHeaders
+				.Should().ContainKey(Headers.CorrelationSequence)
+				.WhichValue
+				.Should().Be("0");
+		}
+
+		[Theory]
+		[InlineData(null, 1)]
+		[InlineData(0, 1)]
+		[InlineData(1, 2)]
+		[InlineData(5, 6)]
+		public async Task Given_incoming_step_context_has_sequence_it_should_increment(int? incomingSequenceNr, int expectedOutgoingSequenceNr)
+		{
+			var incomingHeaders = new Dictionary<string, string>();
+			if (incomingSequenceNr.HasValue)
+			{
+				incomingHeaders.Add(Headers.CorrelationSequence, incomingSequenceNr.ToString());
+			}
+			var incomingStepContext = new IncomingStepContext(
+				new TransportMessage(
+					incomingHeaders, new byte[0]),
+				_transactionContextMock.Object
+			);
+			incomingStepContext.Save(new Message(incomingHeaders, new { }));
+			_stepContext.Save(incomingStepContext);
+
+			// Act
+			await _sut.Process(_stepContext, _next);
+
+			// Assert
+			_messageHeaders
+				.Should().ContainKey(Headers.CorrelationSequence)
+				.WhichValue
+				.Should().Be(expectedOutgoingSequenceNr.ToString());
+		}
+	}
+}
